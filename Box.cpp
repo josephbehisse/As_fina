39829d6c2453f8e8c7ddc9cc282e80af98ad9f4d
josephbehisse <josephbehisse@hotmail.com>
2021-01-03 10:11:58 +0100

    add Box.cpp

---
+++ Box.cpp
@@ -0,0 +1,11 @@
+#include "Box.h"
+
+Box::Box(std::string data_line)
+{
+	std::cout << "Box has been created" << std::endl;
+}
+
+void Box::show_data()
+{
+	std::cout << "Box data is shown" << std::endl;
+}
