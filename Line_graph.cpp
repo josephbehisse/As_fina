8d8c2e929bb1d20dd1bf250204ece712d6523e42
josephbehisse <josephbehisse@hotmail.com>
2021-01-03 10:35:24 +0100

    add Line_graph.cpp

---
+++ Line_graph.cpp
@@ -0,0 +1,11 @@
+#include "Line_graph.h"
+
+Line_graph::Line_graph()
+{
+	std::cout << "Line_graph has been created" << std::endl;
+}
+
+void Line_graph:: show_graph()
+{
+	std::cout <<"my Line_graph is showed" << std::endl;
+}
